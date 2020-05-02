f86360b09e5c12d640ccc5394286c1bde77494f5
dahunkim <kim23222@naver.com>
2020-05-03 01:42:33 +0900

    [테스트] PostgreSQL 연동 테스트
    PostgreSQL 연동 테스트, 테이블 생성, 데이터 삽입

--- springbootHttps/src/main/java/xyz/springboot/PostgreSQL/PostgreSQL.java
+++ springbootHttps/src/main/java/xyz/springboot/PostgreSQL/PostgreSQL.java
@@ -1,4 +1,4 @@
-package xyz.springboot.MySQLRunner;
+package xyz.springboot.PostgreSQL;
 
 import java.sql.Connection;
 import java.sql.Statement;
@@ -11,7 +11,8 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-public class MySQLRunner implements ApplicationRunner{
+@Component
+public class PostgreSQL implements ApplicationRunner{
 
 	@Autowired
 	DataSource dataSource;
@@ -19,21 +20,21 @@ public class MySQLRunner implements ApplicationRunner{
 	@Autowired
 	JdbcTemplate jdbctemplate;
 	
-	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 
 		try(Connection connection = dataSource.getConnection()){
 			System.out.println(dataSource.getClass());
+			System.out.println(connection.getMetaData().getDriverName());
 			System.out.println(connection.getMetaData().getURL());
 			System.out.println(connection.getMetaData().getUserName());
 			
 			Statement createStatement = connection.createStatement();
-			String sql ="CREATE TABLE USER(ID INTEGER NOT NULL, NAME VARCHAR(255),PRIMARY KEY(ID))";
+			String sql ="CREATE TABLE account(ID INTEGER NOT NULL, NAME VARCHAR(255),PRIMARY KEY(ID))";
 			createStatement.execute(sql);
 		}
 		
-	jdbctemplate.execute("insert into user values(1,'dahun')");
+	jdbctemplate.execute("insert into account values(1,'dahun')");
 		
 	}
 }
