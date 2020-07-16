28ab05b56d9a9d20857e3a7b7cbaa0d30a4ea96a
dahunkim <kim23222@naver.com>
2020-07-16 21:54:41 +0900

    [추가] 추가 인증 작성 CustomAuthenticationProvider, FormAuthenticationDetailsSource, FormWebAuthenticationDetails

--- coreSpringSecurity/src/main/java/xyz/dahun/security/provider/CustomAuthenticationProvider.java
+++ coreSpringSecurity/src/main/java/xyz/dahun/security/provider/CustomAuthenticationProvider.java
@@ -3,12 +3,14 @@ package xyz.dahun.security.provider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import xyz.dahun.security.common.FormWebAuthenticationDetails;
 import xyz.dahun.security.service.AccountContext;
 
 public class CustomAuthenticationProvider implements AuthenticationProvider {
@@ -29,6 +31,13 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
 
         if (!passwordEncoder.matches(password, accountContext.getAccount().getPassword())){
             throw new BadCredentialsException("패스워드 인증 실패");
+        }
+
+        FormWebAuthenticationDetails authenticationDetails = (FormWebAuthenticationDetails) authentication.getDetails();
+
+        String secretKey = authenticationDetails.getSecretKey();
+        if(secretKey == null || !"secret".equals(secretKey)){
+            throw new InsufficientAuthenticationException("인증 예외 실패");
         }
 
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(accountContext.getAccount(), null, accountContext.getAuthorities());
