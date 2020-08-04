f1cafcb146e8882231256561d5b5a29bfa7e8ecd
dahunkim <kim23222@naver.com>
2020-08-04 22:55:03 +0900

    [변경] 권한 설정 변경 message 컨트롤러 method변경

--- coreSpringSecurity/src/main/java/xyz/dahun/controller/user/MessageController.java
+++ coreSpringSecurity/src/main/java/xyz/dahun/controller/user/MessageController.java
@@ -1,5 +1,6 @@
 package xyz.dahun.controller.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,14 +9,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MessageController {
 
-    @PostMapping("/messages")
+    @GetMapping("/messages")
     public String message() throws Exception{
         return "user/messages";
     }
 
     @PostMapping("/api/messages")
     @ResponseBody
-    public String apiMessage(){
-        return "messages ok";
+    public ResponseEntity<String> apiMessage() throws Exception{
+        return ResponseEntity.ok().body("ok");
     }
 }
